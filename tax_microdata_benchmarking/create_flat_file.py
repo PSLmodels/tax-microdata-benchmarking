8ef38526e5ac0da6bb4aade42acc32b71a32e686
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-02-08 16:39:26 +0000

    Fix data type issue

--- tax_microdata_benchmarking/create_flat_file.py
+++ tax_microdata_benchmarking/create_flat_file.py
@@ -30,13 +30,15 @@ class tc_MARS(TaxCalcVariableAlias):
             "WIDOW": 5,
         }
         return pd.Series(filing_status).map(CODE_MAP)
-    
+
+
 class tc_e00200p(TaxCalcVariableAlias):
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
         employment_income = person("employment_income", period)
         is_tax_unit_head = person("is_tax_unit_head", period)
         return tax_unit.sum(employment_income * is_tax_unit_head)
+
 
 class tc_e00200s(TaxCalcVariableAlias):
     def formula(tax_unit, period, parameters):
@@ -45,11 +47,13 @@ class tc_e00200s(TaxCalcVariableAlias):
         is_tax_unit_spouse = person("is_tax_unit_spouse", period)
         return tax_unit.sum(employment_income * is_tax_unit_spouse)
 
+
 class tc_e00200(TaxCalcVariableAlias):
     adds = [
         "tc_e00200p",
         "tc_e00200s",
     ]
+
 
 class taxcalc_extension(Reform):
     def apply(self):
@@ -70,7 +74,12 @@ def create_flat_file():
 
     for variable in sim.tax_benefit_system.variables:
         if variable.startswith("tc_"):
-            df[variable[3:]] = sim.calculate(variable)
+            df[variable[3:]] = sim.calculate(variable).values.astype(
+                np.float64
+            )
+
+    # Extra quality-control checks to do with different data types, nothing major
+    df.e00200 = df.e00200p + df.e00200s
 
     df.to_csv("tax_microdata.csv.gz", index=False, compression="gzip")
 
