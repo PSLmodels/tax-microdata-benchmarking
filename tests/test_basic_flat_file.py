87b456e2b4a7b344e964354c4130c903fce17ece
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-04-22 23:30:50 +0100

    Format

--- tests/test_basic_flat_file.py
+++ tests/test_basic_flat_file.py
@@ -9,21 +9,26 @@ FOLDER = Path(__file__).parent
 with open(FOLDER / "tc_variable_totals.yaml") as f:
     tc_variable_totals = yaml.safe_load(f)
 
-with open(FOLDER.parent / "tax_microdata_benchmarking" / "taxcalc_variable_metadata.yaml") as f:
+with open(
+    FOLDER.parent
+    / "tax_microdata_benchmarking"
+    / "taxcalc_variable_metadata.yaml"
+) as f:
     taxcalc_variable_metadata = yaml.safe_load(f)
 
 EXEMPTED_VARIABLES = [
     "DSI"
-    "EIC" # PUF-PE file almost certainly more correct by including CPS data
+    "EIC"  # PUF-PE file almost certainly more correct by including CPS data
     "MIDR",
-    "RECID", # No reason to compare.
-    "a_lineno", # No reason to compare.
-    "agi_bin", # No reason to compare.
-    "blind_spouse" # Issue here but deprioritized.
-    "cmbtp", # No reason to compare.
-    "data_source", # No reason to compare.
-    "s006", # No reason to compare.
+    "RECID",  # No reason to compare.
+    "a_lineno",  # No reason to compare.
+    "agi_bin",  # No reason to compare.
+    "blind_spouse"  # Issue here but deprioritized.
+    "cmbtp",  # No reason to compare.
+    "data_source",  # No reason to compare.
+    "s006",  # No reason to compare.
 ]
+
 
 def pytest_namespace():
     return {"flat_file": None}
@@ -39,6 +44,7 @@ def test_flat_file_builds():
 
     pytest.flat_file = flat_file
 
+
 @pytest.mark.dependency(depends=["test_flat_file_builds"])
 @pytest.mark.parametrize("variable", tc_variable_totals.keys())
 def test_tc_variable_totals(variable):
@@ -52,4 +58,6 @@ def test_tc_variable_totals(variable):
     if tc_variable_totals[variable] == 0:
         # If the taxdata file has a zero total, we'll assume the PE file is still correct.
         return
-    assert abs(total / tc_variable_totals[variable] - 1) < 0.5, f"{variable} ({name}) is off by {total / tc_variable_totals[variable] - 1:.1%}"
+    assert (
+        abs(total / tc_variable_totals[variable] - 1) < 0.5
+    ), f"{variable} ({name}) is off by {total / tc_variable_totals[variable] - 1:.1%}"
