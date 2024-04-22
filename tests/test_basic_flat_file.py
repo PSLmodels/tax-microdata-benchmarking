b0d8b8996c6d8deb2f8341222e9f629f72429638
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-04-22 22:32:38 +0100

    Add init.py

--- tests/test_basic_flat_file.py
+++ tests/test_basic_flat_file.py
@@ -2,6 +2,7 @@ import os
 
 test_mode = os.environ.get("TEST_MODE", "lite")
 
+
 def test_flat_file_runs():
     from tax_microdata_benchmarking.create_flat_file import (
         create_stacked_flat_file,
