a5a8d3c65541835730609341b887d88489ac567c
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-04-22 22:46:38 +0100

    Add taxable pension income test

--- tests/test_basic_flat_file.py
+++ tests/test_basic_flat_file.py
@@ -1,11 +1,28 @@
 import os
+import pytest
 
 test_mode = os.environ.get("TEST_MODE", "lite")
 
 
-def test_flat_file_runs():
+def pytest_namespace():
+    return {"flat_file": None}
+
+
+@pytest.mark.dependency()
+def test_flat_file_builds():
     from tax_microdata_benchmarking.create_flat_file import (
         create_stacked_flat_file,
     )
 
-    create_stacked_flat_file(2021, reweight=test_mode == "full")
+    flat_file = create_stacked_flat_file(2021, reweight=test_mode == "full")
+
+    pytest.flat_file = flat_file
+
+
+@pytest.mark.dependency(depends=["test_flat_file_builds"])
+def test_taxable_pension_income():
+    flat_file = pytest.flat_file
+    weight = flat_file.s006
+    pension_income = flat_file.e01700
+    total_pension_income = (pension_income * weight).sum()
+    assert abs(total_pension_income / 750e9 - 1) < 0.05
