5ae58416253865422cd757176a7392ced4463ea3
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-04-22 23:10:36 +0100

    Add automated aggregate tests for every taxdata variable

--- tests/test_basic_flat_file.py
+++ tests/test_basic_flat_file.py
@@ -1,7 +1,13 @@
 import os
 import pytest
+import yaml
+from pathlib import Path
 
 test_mode = os.environ.get("TEST_MODE", "lite")
+
+FOLDER = Path(__file__).parent
+with open(FOLDER / "tc_variable_totals.yaml") as f:
+    tc_variable_totals = yaml.safe_load(f)
 
 
 def pytest_namespace():
@@ -26,3 +32,12 @@ def test_taxable_pension_income():
     pension_income = flat_file.e01700
     total_pension_income = (pension_income * weight).sum()
     assert abs(total_pension_income / 750e9 - 1) < 0.05
+
+
+@pytest.mark.dependency(depends=["test_flat_file_builds"])
+@pytest.mark.parametrize("variable", tc_variable_totals.keys())
+def test_tc_variable_totals(variable):
+    flat_file = pytest.flat_file
+    weight = flat_file.s006
+    total = (flat_file[variable] * weight).sum()
+    assert abs(total / tc_variable_totals[variable] - 1) < 0.5
