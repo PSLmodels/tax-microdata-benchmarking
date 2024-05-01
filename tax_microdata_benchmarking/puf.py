b5b1465ce0589955e53e5cb32a38d69e2d739200
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-05-01 10:48:18 +0100

    Fix tests

--- tax_microdata_benchmarking/puf.py
+++ tax_microdata_benchmarking/puf.py
@@ -1,7 +1,8 @@
 from tax_microdata_benchmarking.cloud import download_gh_release_asset
 
+
 def download_puf():
-    download_gh_release_asset(
+    return download_gh_release_asset(
         repo="nikhilwoodruff/tax-microdata-benchmarking-releases",
         release_name="puf-2015",
         asset_name="puf_2015.csv.gz",
