98729f4361140afd74687232b916b85a578a2f22
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-05-14 07:59:56 +0100

    Enable taxdata file to run

--- tax_microdata_benchmarking/datasets/taxdata.py
+++ tax_microdata_benchmarking/datasets/taxdata.py
@@ -4,6 +4,7 @@ Any code relating to the taxdata dataset will go here if needed.
 
 from tax_microdata_benchmarking.storage import STORAGE_FOLDER
 from tax_microdata_benchmarking.utils.taxcalc import add_taxcalc_outputs
+from tax_microdata_benchmarking.utils.cloud import download_gh_release_asset
 
 
 def load_taxdata_puf(time_period: int = 2023):
@@ -18,5 +19,12 @@ def load_taxdata_puf(time_period: int = 2023):
     """
     import pandas as pd
 
-    td = pd.read_csv(STORAGE_FOLDER / "input" / "tc23.csv")
+    tc_path = STORAGE_FOLDER / "input" / "tc23.csv"
+    if not tc_path.exists():
+        td = download_gh_release_asset(
+            "nikhilwoodruff/tax-microdata-benchmarking-releases", "taxdata-puf-2023", "tc23.csv",
+        )
+        td.to_csv(tc_path, index=False)
+    else:
+        td = pd.read_csv(STORAGE_FOLDER / "input" / "tc23.csv")
     return add_taxcalc_outputs(td, time_period)
