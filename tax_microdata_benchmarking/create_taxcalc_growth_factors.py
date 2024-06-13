ebfeb00ad8efff47b3859c51f033612d833beb98
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-06-13 14:43:50 -0400

    Revise create_taxcalc_growth_factors.py script

--- tax_microdata_benchmarking/create_taxcalc_growth_factors.py
+++ tax_microdata_benchmarking/create_taxcalc_growth_factors.py
@@ -1,5 +1,5 @@
 """
-Construct growfactors.csv, a Tax-Calculator-style GrowFactors file that
+Construct tmd_growfactors.csv, a Tax-Calculator-style GrowFactors file that
 extends through LAST_YEAR from the puf_growfactors.csv file, which is a
 copy of the most recent growfactors.csv file in the Tax-Calculator repository.
 """
@@ -7,9 +7,10 @@ copy of the most recent growfactors.csv file in the Tax-Calculator repository.
 import pandas as pd
 from tax_microdata_benchmarking.storage import STORAGE_FOLDER
 
+FIRST_YEAR = 2021
 LAST_YEAR = 2074
 PGFFILE = STORAGE_FOLDER / "input" / "puf_growfactors.csv"
-TGFFILE = STORAGE_FOLDER / "output" / "growfactors.csv"
+TGFFILE = STORAGE_FOLDER / "output" / "tmd_growfactors.csv"
 
 
 def create_factors_file():
@@ -18,7 +19,15 @@ def create_factors_file():
     """
     # read PUF-factors from PGFFILE
     gfdf = pd.read_csv(PGFFILE)
+    first_puf_year = gfdf.YEAR.iat[0]
     last_puf_year = gfdf.YEAR.iat[-1]
+
+    # drop gfdf rows before FIRST_YEAR
+    drop_row_index_list = range(0, FIRST_YEAR - first_puf_year)
+    gfdf.drop(drop_row_index_list, inplace=True)
+
+    # set all FIRST_YEAR growfactors to one
+    gfdf.iloc[0, 1:] = 1.0
 
     # add rows thru LAST_YEAR by copying values for last year in PUF file
     if LAST_YEAR > last_puf_year:
