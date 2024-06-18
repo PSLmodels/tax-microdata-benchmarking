8d671932236b6089de2593707e3f658e0f083313
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-06-18 20:25:18 +0100

    Fix download location

--- tax_microdata_benchmarking/download_prerequisites.py
+++ tax_microdata_benchmarking/download_prerequisites.py
@@ -4,7 +4,7 @@ from tax_microdata_benchmarking.storage import STORAGE_FOLDER
 
 def download_prerequisites():
     puf_2015 = download_gh_release_asset(
-        repo="nikhilwoodruff/tax-microdata-benchmarking/releases",
+        repo="nikhilwoodruff/tax-microdata-benchmarking-releases",
         release_name="tmd-prerequisites",
         asset_name="puf_2015.csv",
     )
@@ -12,7 +12,7 @@ def download_prerequisites():
     puf_2015.to_csv(STORAGE_FOLDER / "input" / "puf_2015.csv", index=False)
 
     demographics_2015 = download_gh_release_asset(
-        repo="nikhilwoodruff/tax-microdata-benchmarking/releases",
+        repo="nikhilwoodruff/tax-microdata-benchmarking-releases",
         release_name="tmd-prerequisites",
         asset_name="demographics_2015.csv",
     )
