2c92e5e58325bf6c74a689af25bb781c60aa710a
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-06-22 14:17:14 -0400

    Fix typo in puf.py module

--- tax_microdata_benchmarking/datasets/puf.py
+++ tax_microdata_benchmarking/datasets/puf.py
@@ -176,7 +176,7 @@ def preprocess_puf(puf: pd.DataFrame) -> pd.DataFrame:
     # Ignore cmbtp (estimate of AMT income not in AGI)
     # Ignore k1bx14s and k1bx14p (partner self-employment income included in partnership and S-corp income)
     qbi = puf.E00900 + puf.E26270 + puf.E02100 + puf.E27200
-    puf["w2_wages_from_qualified_business"] = qbi * W2_WAGE_SCALE
+    puf["w2_wages_from_qualified_business"] = qbi * W2_WAGES_SCALE
 
     puf["filing_status"] = puf.MARS.map(
         {
