a1cbc2abcc914bc2c2970cafaa9048dee30e367b
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-06-22 14:29:54 -0400

    Update W2_WAGES_SCALE imputation assumption

--- tax_microdata_benchmarking/imputation_assumptions.py
+++ tax_microdata_benchmarking/imputation_assumptions.py
@@ -2,4 +2,4 @@
 Central location for data imputation assumptions.
 """
 
-W2_WAGES_SCALE = 0.19980  # used to impute pass-through W-2 wages
+W2_WAGES_SCALE = 0.19941  # used to impute pass-through W-2 wages
