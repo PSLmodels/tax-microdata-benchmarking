bb80ba8169c95a19b843ea00d6c9734efc5407e0
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-06-23 16:44:50 -0400

    Make handling of negative QBI consistent between puf.py and qbi.py

--- tax_microdata_benchmarking/datasets/puf.py
+++ tax_microdata_benchmarking/datasets/puf.py
@@ -180,7 +180,7 @@ def preprocess_puf(puf: pd.DataFrame) -> pd.DataFrame:
     # Ignore f2441 (AMT form attached)
     # Ignore cmbtp (estimate of AMT income not in AGI)
     # Ignore k1bx14s and k1bx14p (partner self-employment income included in partnership and S-corp income)
-    qbi = puf.E00900 + puf.E26270 + puf.E02100 + puf.E27200
+    qbi = np.maximum(0, puf.E00900 + puf.E26270 + puf.E02100 + puf.E27200)
     puf["w2_wages_from_qualified_business"] = qbi * W2_WAGES_SCALE
 
     puf["filing_status"] = puf.MARS.map(
