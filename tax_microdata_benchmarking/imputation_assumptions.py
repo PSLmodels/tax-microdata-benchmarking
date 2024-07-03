300ca344f80fa641488716ba55ef92abd58130e2
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-07-03 13:36:54 +0100

    Amend default imputation for W2 wages

--- tax_microdata_benchmarking/imputation_assumptions.py
+++ tax_microdata_benchmarking/imputation_assumptions.py
@@ -6,4 +6,4 @@ IMPUTATION_RF_RNG_SEED = 1928374  # random number seed used by RandomForest
 
 IMPUTATION_BETA_RNG_SEED = 37465  # random number seed used for Beta variates
 
-W2_WAGES_SCALE = 0.225620  # parameter used to impute pass-through W-2 wages
+W2_WAGES_SCALE = 0.33  # parameter used to impute pass-through W-2 wages
