52d89a404aea0000ee53ab62f551b7bd806490d3
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-08-20 15:15:54 -0400

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name="tax_microdata_benchmarking",
     version="0.1.0",
     packages=find_packages(),
+    python_requires=">=3.10,<3.13"
     install_requires=[
         "policyengine_us==0.794",
-        "taxcalc>=4.1.0",
+        "taxcalc>=4.2.1",
         "pytest",
         "black>=24.4.2",
         "torch",
