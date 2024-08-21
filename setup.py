7b57f3d276e6910a55c2e781972475865ab3d409
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-08-21 09:44:47 -0400

    Update several expected tax-expenditure estimates

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     packages=find_packages(),
     python_requires=">=3.10,<3.13",
     install_requires=[
-        "policyengine_us==1.45.0",
-        "marshmallow<=3.19.0",  # to work around paramtools bug
-        "taxcalc>=4.2.1",
+        "policyengine_us==1.55.0",
+        "marshmallow<3.22",  # to work around paramtools bug
+        "taxcalc>=4.2.1",  # requires paramtools
         "black>=24.4.2",
-        "tables",
+        "tables",  # required by policyengine_us
         "pytest",
         "torch",
         "tensorboard",
