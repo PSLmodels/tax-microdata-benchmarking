7eff784bb35d18c0d6a589ec22873409cda3fb77
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-08-21 09:03:53 -0400

    Limit marshmallow version in setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     python_requires=">=3.10,<3.13",
     install_requires=[
         "policyengine_us==1.45.0",
+        "marshmallow<=3.19.0",  # to work around paramtools bug
         "taxcalc>=4.2.1",
         "black>=24.4.2",
         "tables",
