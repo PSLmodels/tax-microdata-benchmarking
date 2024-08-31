d94d336b162252e9bd23fe785ae01fda27063066
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-08-31 19:28:35 -0400

    Revise create_taxcalc_growth_factors.py to do 2022 adjustments

--- tmd/create_taxcalc_growth_factors.py
+++ tmd/create_taxcalc_growth_factors.py
@@ -11,8 +11,8 @@ FIRST_YEAR = 2021
 LAST_YEAR = 2074
 
 AWAGE_INDEX = 6
-ASCHEI_INDEX = 9
-AINTS_INDEX = 11
+# ASCHEI_INDEX = 9
+# AINTS_INDEX = 11
 ADIVS_INDEX = 12
 ACGNS_INDEX = 13
 ASOCSEC_INDEX = 14
@@ -38,12 +38,14 @@ def create_factors_file():
     gfdf.iloc[0, 1:] = 1.0
 
     # adjust some factors in order to calibrate tax revenue after FIRST_YEAR
-    gfdf.iat[2022 - FIRST_YEAR, AWAGE_INDEX] += 0.087
-    gfdf.iat[2022 - FIRST_YEAR, ADIVS_INDEX] += -0.10
-    gfdf.iat[2022 - FIRST_YEAR, ACGNS_INDEX] += -0.10
-    gfdf.iat[2022 - FIRST_YEAR, AINTS_INDEX] += 0.0
-    gfdf.iat[2022 - FIRST_YEAR, ASOCSEC_INDEX] += -0.25
-    gfdf.iat[2022 - FIRST_YEAR, ASCHEI_INDEX] += -0.27
+    # ... adjust 2022 factors to hit 2023 tax revenue targets:
+    # ...... adjustments to hit 2023 PTAX target of 1591.3
+    gfdf.iat[2022 - FIRST_YEAR, AWAGE_INDEX] += 0.099
+    # ...... adjustments to hit 2023 ITAX target of 2505.3
+    gfdf.iat[2022 - FIRST_YEAR, ADIVS_INDEX] += -0.036
+    gfdf.iat[2022 - FIRST_YEAR, ACGNS_INDEX] += -0.036
+    gfdf.iat[2022 - FIRST_YEAR, ASOCSEC_INDEX] += -0.18
+    # ... adjust 202? factors to hit 2026 targets:
 
     # add rows thru LAST_YEAR by copying values for last year in PUF file
     if LAST_YEAR > last_puf_year:
