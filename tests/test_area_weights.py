b06655412ec28525bce93dba5c0bb28dd7274bf6
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-09-12 19:43:11 -0400

    Update test_area_weights.py

--- tests/test_area_weights.py
+++ tests/test_area_weights.py
@@ -5,9 +5,9 @@ Tests of tmd/areas/create_area_weights.py script.
 from tmd.areas.create_area_weights import create_area_weights_file
 
 
-def test_area_zz():
+def test_area_bb():
     """
-    Optimize national weights for the faux zz area targets.
+    Optimize national weights for the faux bb area targets.
     """
     loss = create_area_weights_file("bb", write_file=False)
-    assert loss < 1e-20
+    assert loss < 1e-7
