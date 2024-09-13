3034d3400463d4b6b86e14dcc25e40f4ba273362
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-09-13 09:48:46 -0400

    Revise test_area_weights.py to expect target RMSE return value

--- tests/test_area_weights.py
+++ tests/test_area_weights.py
@@ -7,7 +7,7 @@ from tmd.areas.create_area_weights import create_area_weights_file
 
 def test_area_bb():
     """
-    Optimize national weights for the faux bb area targets.
+    Optimize national weights using the faux bb area targets.
     """
-    loss = create_area_weights_file("bb", write_file=False)
-    assert loss < 1e-7
+    rmse = create_area_weights_file("bb", write_file=False)
+    assert rmse < 1e-3
