c8a473a9b59bf9e2c143392fd6835eb18fdc5eb7
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-09-16 09:42:35 -0400

    Revise test_area_weights.py and add bb.log-expect

--- tests/test_area_weights.py
+++ tests/test_area_weights.py
@@ -2,12 +2,29 @@
 Tests of tmd/areas/create_area_weights.py script.
 """
 
+import sys
+from difflib import context_diff
+import pytest
+from tmd.areas import AREAS_FOLDER
 from tmd.areas.create_area_weights import create_area_weights_file
 
 
+# @pytest.skip
 def test_area_bb():
     """
-    Optimize national weights using the faux bb area targets.
+    Optimize national weights for faux bb area using the faux bb area targets.
     """
-    rmse = create_area_weights_file("bb", write_file=False)
-    assert rmse < 1e-4
+    # write area/weights/bb_tmd_weights.csv.gz and area/weights/bb.log files
+    create_area_weights_file("bb", write_file=True)
+    # compare area/weights/bb.log file with area/weights/bb.log-expect file
+    wpath = AREAS_FOLDER / "weights"
+    with open(wpath / "bb.log", "r", encoding="utf-8") as afile:
+        act = afile.readlines()
+    with open(wpath / "bb.log-expect", "r", encoding="utf-8") as efile:
+        exp = efile.readlines()
+    diffs = list(
+        context_diff(act, exp, fromfile='ACTUAL', tofile='EXPECT', n=0)
+    )
+    if len(diffs) > 0:
+        sys.stdout.writelines(diffs)
+        raise ValueError("ACT vs EXP differences for area/weights/bb.log")
