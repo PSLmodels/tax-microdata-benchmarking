6d3d451234b6eed353b6c2848a2e81aa62e2718a
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-09-16 10:07:19 -0400

    Add test_area_make.py test module

--- tests/test_area_weights.py
+++ tests/test_area_weights.py
@@ -2,29 +2,12 @@
 Tests of tmd/areas/create_area_weights.py script.
 """
 
-import sys
-from difflib import context_diff
-import pytest
-from tmd.areas import AREAS_FOLDER
 from tmd.areas.create_area_weights import create_area_weights_file
 
 
-# @pytest.skip
 def test_area_bb():
     """
     Optimize national weights for faux bb area using the faux bb area targets.
     """
-    # write area/weights/bb_tmd_weights.csv.gz and area/weights/bb.log files
-    create_area_weights_file("bb", write_file=True)
-    # compare area/weights/bb.log file with area/weights/bb.log-expect file
-    wpath = AREAS_FOLDER / "weights"
-    with open(wpath / "bb.log", "r", encoding="utf-8") as afile:
-        act = afile.readlines()
-    with open(wpath / "bb.log-expect", "r", encoding="utf-8") as efile:
-        exp = efile.readlines()
-    diffs = list(
-        context_diff(act, exp, fromfile="ACTUAL", tofile="EXPECT", n=0)
-    )
-    if len(diffs) > 0:
-        sys.stdout.writelines(diffs)
-        raise ValueError("ACT vs EXP differences for area/weights/bb.log")
+    rmse = create_area_weights_file("bb", write_file=False)
+    assert rmse < 1e-4
