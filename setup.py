7c3e54018e81f332ff957baf70292510fb4ba88b
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-09-16 13:49:11 -0400

    Reguire taxcalc version 4.2.2 or higher in setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="tmd",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     python_requires=">=3.10,<3.13",
     install_requires=[
         "policyengine_us==1.55.0",
         "tables",  # required by policyengine_us
-        "marshmallow<3.22",  # to work around paramtools bug
-        "taxcalc>=4.2.1",  # requires paramtools
+        "taxcalc>=4.2.2",
         "scikit-learn",
         "torch",
         "tensorboard",
