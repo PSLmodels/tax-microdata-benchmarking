7f9f5e148c3094e1a31341e80ba91764a6f9e51d
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-09-17 09:13:01 -0400

    Add test_tmd_stats.py test module

---
+++ tests/test_tmd_stats.py
@@ -0,0 +1,42 @@
+"""
+Test of tmd/storage/output/tmd.csv.gz content.
+"""
+
+import sys
+from difflib import context_diff
+import pytest
+
+
+# @pytest.mark.skip
+def test_tmd_stats(tmd_variables, tests_folder):
+    """
+    Create tests/tmd.stats-actual and compare with tests/tmd.stats-expect.
+    """
+    # create tmd.stats-actual file
+    actual_path = tests_folder / "tmd.stats-actual"
+    with open(actual_path, "w", encoding="utf-8") as actual_file:
+        for vname in sorted(tmd_variables.columns):
+            stats = tmd_variables[vname].describe().to_dict()
+            for key, val in stats.items():
+                actual_file.write(f"{vname}  {key}  {val}\n")
+    # compare actual file with expect file
+    with open(actual_path, "r", encoding="utf-8") as afile:
+        act = afile.readlines()
+    expect_path = tests_folder / "tmd.stats-expect"
+    with open(expect_path, "r", encoding="utf-8") as efile:
+        exp = efile.readlines()
+    diffs = list(
+        context_diff(act, exp, fromfile="ACTUAL", tofile="EXPECT", n=0)
+    )
+    if len(diffs) > 0:
+        sys.stdout.write(">>>>> ACTUAL FILE:\n")
+        sys.stdout.write("------------------------------------------------\n")
+        sys.stdout.writelines(act)
+        sys.stdout.write("------------------------------------------------\n")
+        sys.stdout.write(">>>>> DIFFS FILE:\n")
+        sys.stdout.write("------------------------------------------------\n")
+        sys.stdout.writelines(diffs)
+        sys.stdout.write("------------------------------------------------\n")
+        actual_path.unlink()
+        raise ValueError("ACTUAL vs EXPECT differences")
+    actual_path.unlink()
