86454005c5213f688b105d04dfe0f49882a69f79
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-09-20 10:18:21 -0400

    Fix test_area_weights.py for area name change

--- tests/test_area_weights.py
+++ tests/test_area_weights.py
@@ -5,9 +5,9 @@ Tests of tmd/areas/create_area_weights.py script.
 from tmd.areas.create_area_weights import create_area_weights_file
 
 
-def test_area_bb():
+def test_area_xx():
     """
-    Optimize national weights for faux bb area using the faux bb area targets.
+    Optimize national weights for faux xx area using the faux xx area targets.
     """
-    rmse = create_area_weights_file("bb", write_log=False, write_file=False)
+    rmse = create_area_weights_file("xx", write_log=False, write_file=False)
     assert rmse < 1e-4
