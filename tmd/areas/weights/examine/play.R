8c03bd971b4cdb35f57d8f8c312cba337edf301e
donboyd5 <donboyd5@gmail.com>
2024-10-22 09:21:31 -0400

    additional checks

--- tmd/areas/weights/examine/play.R
+++ tmd/areas/weights/examine/play.R
@@ -43,3 +43,47 @@ tmp |>
 # 4300 result
 
 
+# compare tmd2021 to djbout -----------------------------------------------
+
+tmd2021 <- readRDS(here::here("temp_data", "tmd2021.rds"))
+
+djbout <- read_csv(here::here("temp_data", "djbout.csv"))
+glimpse(djbout)
+
+baddjb <- djbout |> 
+  mutate(n=n(), .by=RECID) |> 
+  filter(n!=1) |> 
+  arrange(RECID) |> 
+  relocate(RECID, n, data_source, c00100)
+count(baddjb, data_source)
+
+baddjbds0 <- djbout |> 
+  filter(data_source==0) |>  # CPS records
+  mutate(n=n(), .by=RECID) |> 
+  filter(n!=1) |> 
+  arrange(RECID) |> 
+  relocate(RECID, n, data_source, c00100)
+
+baddjbds1 <- djbout |> 
+  filter(data_source==1) |> # PUF records
+  mutate(n=n(), .by=RECID) |> 
+  filter(n!=1) |> 
+  arrange(RECID) |> 
+  relocate(RECID, n, data_source, c00100)
+
+comp <- bind_rows(
+  tmd2021 |> select(RECID, data_source, c00100, s006) |> mutate(src="tmd"),
+  djbout |> select(RECID, data_source, c00100, s006) |> mutate(src="djb")) |> 
+  arrange(RECID, desc(src))
+
+count(comp, src)
+
+bad <- comp |> 
+  mutate(n=n(), .by=RECID) |> 
+  filter(n != 2)
+
+
+comp |> 
+  pivot_longer(-c(RECID, src)) |> 
+  pivot_wider(names_from = src, values_from = value)
+
