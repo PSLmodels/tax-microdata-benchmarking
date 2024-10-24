0d1e284a2e8254c36133c30e0cd8db4ab4ad3d22
donboyd5 <donboyd5@gmail.com>
2024-10-24 04:51:52 -0400

    update play.r to work with debugging info written by create_area_weights.py

--- tmd/areas/weights/examine/play.R
+++ tmd/areas/weights/examine/play.R
@@ -13,9 +13,147 @@ phase4_statecds <- c("AK00", "DE00", "ID01", "ID02", "ME02", "MT00", "ND00", "PA
 # OLD: djbout <- read_csv(here::here("temp_data", "djbout.csv")) # this is tax calc output vdf from create_area_weights.py
 tmd2021 <- readRDS(here::here("temp_data", "tmd2021.rds")) # now based on djbout.csv
 sum(tmd2021$s006) # is what we want to see
+ns(tmd2021)
 tmdbase <- readRDS(here::here("temp_data", "tmd_base.rds"))
 usweights <- readRDS(here::here("temp_data", "us_weights.rds"))
 ns(usweights)
+area_weights <- readRDS(here::here("temp_data", "area_weights.rds"))
+
+
+# debug ak00 agistub 9 c00100 amount (target 10, row 11) --------------------------------------------------------
+
+#.. get data ----
+# djbout 225,256 data rows
+# same for weights
+# same for masks
+
+# ...national_population 334283385.27000004
+# ...scale 2.0948260728756473e-10
+
+# target 10
+national_population <- 334283385.27000004
+cdpopulation <- 732673 # row 2 xtot
+unscaled_target <- 4773666000 # unscaled_target 4773666000 good
+(initial_weights_scale = cdpopulation / national_population)
+# 1 / cdpopulation
+1 / unscaled_target
+scale <- 2.0948260728756473e-10 # good
+(scaled_target <- unscaled_target * scale)
+
+iweights <- read_csv(here::here("temp_data", "unmasked_varray.csv"),
+                col_names = "umv") # this should be c00100
+
+xvalues <- read_csv(here::here("temp_data", "xvalues.csv"),
+                     col_names = "x") # this should be c00100
+
+umv <- read_csv(here::here("temp_data", "unmasked_varray.csv"),
+                col_names = "umv") # this should be c00100
+
+mask <- read_csv(here::here("temp_data", "mask.csv"),
+                 col_names = "mask")
+
+smv <- read_csv(here::here("temp_data", "scaled_masked_varray.csv"),
+                col_names = "smv")
+
+
+# ..check some of the data ------------------------------------------------
+class(umv); class(mask); class(smv)
+
+akweights <- area_weights |> filter(src=="ak00")
+
+combo <- cbind(tmd2021 |> 
+                 select(data_source, s006, XTOT, c00100), 
+               xvalues, umv, mask, smv,
+               akweights |> select(fweight=WT2021)
+               ) |> 
+  mutate(row=row_number(),
+         iweight = s006 * initial_weights_scale) |> 
+  relocate(row)
+
+# population check - good
+combo |> 
+  summarise(pop=sum(s006 * XTOT)) # 334,283,385 vs expected 334,283,385.27000004
+
+# c00100 sums - good
+combo |> 
+  mutate(diff = umv - c00100,
+         pdiff = diff / c00100) |> 
+  filter(pdiff != 0) |> 
+  arrange(desc(abs(pdiff))) # super minor diffs
+
+sum(umv) # 638131482635
+sum(tmd2021$c00100) # 638131482635
+
+# mask check
+combo |> 
+  mutate(maskcheck = c00100 )
+
+# final weights check
+check <- combo |> 
+  mutate(fweight_check = iweight * x,
+         diff=fweight_check - fweight,
+         pdiff=diff / fweight)
+check |> arrange(desc(abs(pdiff)))
+check |> 
+  summarise(fweight=sum(fweight),
+            fweight_check=sum(fweight), 
+            .by=data_source)
+
+
+# smv looks good
+smvcheck <- combo |> 
+  mutate(smvcheck = c00100 * mask * scale,
+         diff = smv - smvcheck,
+         pdiff = diff / smvcheck) |> 
+  filter(pdiff != 0)
+sum(abs(smvcheck$pdiff)) # 1.118748e-12
+  
+smvcheck <- tmd2021$c00100 * mask * scale
+class(smvcheck)
+sum(smv) # 130.6638
+sum(smvcheck) # 130.6638
+
+tibble(smv=as.numeric(smv), check=as.numeric(smvcheck)) |> 
+  mutate(diff=smv - check) |> 
+  filter(diff != 0)
+
+
+
+sum(mask$mask)
+smv |> 
+  mutate(nz=(smv != 0) * 1) |> 
+  summarise(n=n(), smv=sum(smv), .by=nz)
+
+nrow(mask)
+nrow(smv)
+
+combo |> 
+  filter(mask == 1) |> 
+  mutate(fweight_round=round(iweight * x, 2)) |> 
+  summarise(totmod=sum(umv * fweight),
+            totcalc=sum(c00100 * iweight * x),
+            totcalc_round=sum(c00100 * fweight_round))
+
+# 4,773,666,000
+
+
+
+
+# weights analysis --------------------------------------------------------
+
+check <- combo |> 
+  mutate(fweight_calc = iweight * x,
+         fweight_calcround = round(fweight, 2),
+         rnddiff=fweight - fweight_calcround,
+         rndpdiff=rnddiff / fweight_calcround,
+         calcdiff=fweight - fweight_calc,
+         calcpdiff=calcdiff / fweight_calc)
+
+check |> 
+  summarise(mdns006=median(s006),
+            mdnfwcalc=median(fweight_calc),
+            mdnfweight=median(fweight),
+            .by=mask)
 
 
 # analysis ----------------------------------------------------------------
