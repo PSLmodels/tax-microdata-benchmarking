8deb45e5d5b09d7445a81cf13023836e7b7622e1
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-10-28 19:13:38 -0400

    Make NUM_BINS an input to chisquare_test.py

--- tmd/areas/chisquare_test.py
+++ tmd/areas/chisquare_test.py
@@ -1,7 +1,8 @@
 """
-Conduct chi-square test of the similarity of two weights distributions
-for the same area that are generated using different targets.  The two area
-weights files are assumed to be located in the tmd/areas/weights folder.
+Conduct chi-square test of the similarity of two distributions of weights by
+income tax liability categories for the same area that are generated using
+different targets.  The two area weights files are assumed to be located in
+the tmd/areas/weights folder.
 All test output is written to stdout; error messages are written to stderr.
 
 For background information on the chi-square test that compares two
@@ -15,6 +16,7 @@ https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.chi2_contingenc
 USAGE: python chisquare_test.py WGHT1 WGHT2
 """
 
+import os
 import sys
 from pathlib import Path
 import numpy as np
@@ -33,6 +35,8 @@ ITAX_BINS = [-9e99] + B_N1 + B_N2 + B_LO + B_H1 + B_H2 + [9e99]
 CACHED_ITAX_PATH = STORAGE_FOLDER / "output" / "cached_iitax.npy"
 
 TAX_YEAR = 2021
+
+DEFAULT_NUM_BINS = 2000
 
 DUMP_DETAILS = False
 
@@ -75,8 +79,39 @@ def weights_array(wghts_path: Path):
     Return array containing TAX_YEAR weights in area weights file with wpath.
     """
     wdf = pd.read_csv(wghts_path)
-    warray = wdf[f"WT{TAX_YEAR}"] * 0.01
+    warray = wdf[f"WT{TAX_YEAR}"]
     return warray
+
+
+def weighted_qcut(values, weights, numbins):
+    """
+    Return weighted quantile cuts from a given series, values.
+    """
+    quantiles = np.linspace(0, 1, numbins + 1)
+    order = weights.iloc[values.argsort()].cumsum()
+    bins = pd.cut(order / order.iloc[-1], quantiles, right=True, labels=False)
+    return bins.sort_index()
+
+
+def sorted_vdf_with_itxbin(vdf: pd.DataFrame):
+    """
+    Expects vdf to include two area weights arrays and an itax array.
+    Returns itax-sorted vdf with added itxbin given NUM_BINS environ variable.
+    """
+    # determine number of itax bins to construct
+    numbins = DEFAULT_NUM_BINS
+    if "NUM_BINS" in os.environ:
+        numbins = int(os.environ["NUM_BINS"])
+    # check contents of vdf
+    assert "wght1" in vdf, "wght1 variable not in vdf"
+    assert "wght2" in vdf, "wght2 variable not in vdf"
+    assert "itax" in vdf, "itax variable not in vdf"
+    # add itxbin variable to vdf
+    avg_wght = 0.5 * (vdf.wght1 + vdf.wght2)
+    vdf["itxbin"] = weighted_qcut(vdf.itax, avg_wght, numbins)
+    # return sorted vdf
+    vdf.sort_values("itax", inplace=True)
+    return vdf
 
 
 # -- High-level logic of the script:
@@ -88,53 +123,39 @@ def main(wname1: str, wpath1: Path, wname2: str, wpath2: Path):
     and the WGHT2 weights (wpath2), which are weights for the same area
     generated using different targets.
     """
-    if DUMP_DETAILS:
-        for idx, low_edge in enumerate(ITAX_BINS):
-            print(idx, low_edge)
-
     # read cached iitax amounts, which are the same for each area weights file
-    iitax = np.load(CACHED_ITAX_PATH)
+    itax = np.load(CACHED_ITAX_PATH)
+
+    # construct vdf DataFrame and sort it and add itxbin variable to it
     wght1 = weights_array(wpath1)
     wght2 = weights_array(wpath2)
-    assert len(wght1) == len(wght2) == len(iitax)
-    witax1 = (wght1 * iitax).sum() * 1e-9
-    witax2 = (wght2 * iitax).sum() * 1e-9
+    assert len(wght1) == len(wght2) == len(itax)
+    witax1 = (wght1 * itax).sum() * 1e-9
+    witax2 = (wght2 * itax).sum() * 1e-9
     witax_ratio = witax2 / witax1
-
-    # create iitax bin variable called itxbin
-    itxbin = pd.cut(iitax, bins=ITAX_BINS, right=False)
-    assert len(itxbin) == len(wght1)
-
-    # compute total weight in each iitax bin
-    df1 = pd.DataFrame({"iitax": iitax, "s006": wght1, "itxbin": itxbin})
-    df2 = pd.DataFrame({"iitax": iitax, "s006": wght2, "itxbin": itxbin})
-    wght1_bin = df1.groupby("itxbin", observed=True)["s006"].sum()
-    wght2_bin = df2.groupby("itxbin", observed=True)["s006"].sum()
-    assert len(wght1_bin) == len(wght2_bin)
-    num_bins = len(wght1_bin)
-    if DUMP_DETAILS:
-        wght1_tot = df1.s006.sum()
-        for idx, wght in enumerate(wght1_bin):
-            pct = 100 * wght / wght1_tot
-            print(f"{idx:3d}  {wght:10.2f}  {pct:5.2f}")
+    vdf = pd.DataFrame({"wght1": wght1, "wght2": wght2, "itax": itax})
+    vdf = sorted_vdf_with_itxbin(vdf)
 
     # prepare frequency data for chi-square test
-    unweighted_count = len(df1)
-    wght1_sum = df1.s006.sum()
-    wght2_sum = df2.s006.sum()
+    unweighted_count = len(vdf)
+    wght1_sum = wght1.sum()
+    wght2_sum = wght2.sum()
     weight_ratio = wght2_sum / wght1_sum
     print(f"{wname2}/{wname1}_weight_total_ratio= {weight_ratio:.4f}")
     print(
         f"{wname1},{wname2}_weighted_iitax($B)= {witax1:.3f} {witax2:.3f}"
         f"  ===>  ratio= {witax_ratio:.4f}"
     )
-    freq1 = unweighted_count * wght1_bin / wght1_sum
-    freq2 = unweighted_count * wght2_bin / wght2_sum
+    gvdf = vdf.groupby(by=["itxbin"], sort=False, observed=True)
+    freq1 = (unweighted_count / wght1_sum) * gvdf["wght1"].sum()
+    freq2 = (unweighted_count / wght2_sum) * gvdf["wght2"].sum()
     assert len(freq1) == len(freq2)
     assert np.allclose([freq1.sum()], [freq2.sum()])
     min_cell_count = min(np.min(freq1), np.min(freq2))
-    print(f"number_of_iitax_bins= {num_bins}")
     print(f"minimum_bin_freqency_count= {min_cell_count:.1f}")
+    if min_cell_count < 5:
+        print("WARNING: reduce value of NUM_BINS environment variable")
+        print("         to get minimum_bin_fredquency_count above five")
 
     # conduct chi-square test
     print("Conducting chi-square two-variable independence test:")
