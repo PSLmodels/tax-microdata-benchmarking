208037d299336301c236c74a1292bada9540ad64
donboyd5 <donboyd5@gmail.com>
2024-10-29 19:01:49 -0400

    add .gitignore, hml, Rproj, qmd and R files

---
+++ tmd/areas/weights/examine/R/constants.R
@@ -0,0 +1,26 @@
+
+# \\wsl.localhost\Ubuntu\home\donboyd5\Documents\python_projects\tax-microdata-benchmarking\tmd\storage\output
+TMDDIR <- here::here("..", "..", "..", "storage", "output")
+# list.files(TMDDIR)
+
+TARGETSDIR <- here::here("..", "..", "targets")
+WEIGHTSDIR <- here::here("..")
+# list.files(TARGETSDIR)
+# list.files(WEIGHTSDIR)
+
+# CDZIPURL <- "https://www.irs.gov/pub/irs-soi/congressional2021.zip"
+# CDDOCURL <- "https://www.irs.gov/pub/irs-soi/21incddocguide.docx"
+
+# \\wsl.localhost\Ubuntu\home\donboyd5\Documents\python_projects\tax-microdata-benchmarking\tmd\areas\weights\examine
+# \\wsl.localhost\Ubuntu\home\donboyd5\Documents\python_projects\tax-microdata-benchmarking\tmd\areas\targets\prepare
+# TARGETSPREPDIR <- here::here("..", "..", "targets", "prepare")
+# print(TARGETSPREPDIR)  # Should print the absolute path to the folder
+# list.files(TARGETSPREPDIR)
+
+# CDDIR <- here::here("cds")
+# CDDIR <- fs::path(TARGETSPREPDIR, "cds")
+# CDRAW <- fs::path(CDDIR, "raw_data")
+# CDINTERMEDIATE <- fs::path(CDDIR, "intermediate")
+# CDFINAL <- fs::path(CDDIR, "final")
+# list.files(CDFINAL)
+# CDDOCEXTRACT <- "cd_documentation_extracted_from_21incddocguide.docx.xlsx"
