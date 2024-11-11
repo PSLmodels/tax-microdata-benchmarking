4f868b4bcd70a883fda18d2fa88e677270b7d6d6
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-11-11 15:38:48 -0500

    Remove obsolete app.py script

--- tests/test_tax_revenue.py
+++ tests/test_tax_revenue.py
@@ -42,7 +42,7 @@ def test_tax_revenue(
         exp_itax[year] = round(fy2cy(fy_itax[year], fy_itax[year + 1]), 3)
         exp_ptax[year] = round(fy2cy(fy_ptax[year], fy_ptax[year + 1]), 3)
     # calculate actual tax revenues for each calendar year
-    pol = tc.Policy.tmd_constructor(tmd_growfactors_path)
+    pol = tc.Policy()
     wghts = str(tmd_weights_path)
     growf = tc.GrowFactors(growfactors_filename=str(tmd_growfactors_path))
     input_data = tc.Records(
