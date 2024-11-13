020a579c485fa7f17603f9bf1b5b13f8a2035cb0
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-11-13 10:25:02 -0500

    Update tmd/examination/results4.md using Tax-Calculator 4.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tmd",
-    version="0.5.1",
+    version="0.6.0",
     packages=find_packages(),
     python_requires=">=3.10,<3.13",
     install_requires=[
