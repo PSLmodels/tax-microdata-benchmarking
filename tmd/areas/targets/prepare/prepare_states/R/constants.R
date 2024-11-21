c3fdc1d50cec107dcd81e712991aa3770e9e2e4f
donboyd5 <donboyd5@gmail.com>
2024-11-21 14:05:04 -0500

    add constants for folders

--- tmd/areas/targets/prepare/prepare_states/R/constants.R
+++ tmd/areas/targets/prepare/prepare_states/R/constants.R
@@ -1,17 +1,23 @@
 
-CDZIPURL <- "https://www.irs.gov/pub/irs-soi/congressional2021.zip"
-CDDOCURL <- "https://www.irs.gov/pub/irs-soi/21incddocguide.docx"
+DRAW <- here::here("data", "data_raw")
+DINTERMEDIATE <- here::here("data", "intermediate")
 
-CDDIR <- here::here("cds")
-CDRAW <- fs::path(CDDIR, "raw_data")
-CDINTERMEDIATE <- fs::path(CDDIR, "intermediate")
-CDFINAL <- fs::path(CDDIR, "final")
-
-CDDOCEXTRACT <- "cd_documentation_extracted_from_21incddocguide.docx.xlsx"
-
-TMDHOME <- fs::path(here::here(), "..", "..", "..", "..", "..")
-# normalizePath(TMDHOME)
-TMDDATA <- fs::path(TMDHOME, "tmd", "storage", "output")
-# normalizePath(TMDDATA)
 
 CDAGICUTS <- c(-Inf, 1, 10e3, 25e3, 50e3, 75e3, 100e3, 200e3, 500e3, Inf)
+
+# CDZIPURL <- "https://www.irs.gov/pub/irs-soi/congressional2021.zip"
+# CDDOCURL <- "https://www.irs.gov/pub/irs-soi/21incddocguide.docx"
+# 
+# CDDIR <- here::here("cds")
+# CDRAW <- fs::path(CDDIR, "raw_data")
+# CDINTERMEDIATE <- fs::path(CDDIR, "intermediate")
+# CDFINAL <- fs::path(CDDIR, "final")
+# 
+# CDDOCEXTRACT <- "cd_documentation_extracted_from_21incddocguide.docx.xlsx"
+# 
+# TMDHOME <- fs::path(here::here(), "..", "..", "..", "..", "..")
+# # normalizePath(TMDHOME)
+# TMDDATA <- fs::path(TMDHOME, "tmd", "storage", "output")
+# # normalizePath(TMDDATA)
+# 
+
