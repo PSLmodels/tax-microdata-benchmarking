f84aebd842fdc73cc31f4d5d3039b78824f98fb2
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-12-15 18:31:07 -0500

    Update setup.py to require taxcalc 4.3.5 or higher

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="tmd",
-    version="0.6.1",
+    version="0.6.2",
     packages=find_packages(),
     python_requires=">=3.10,<3.13",
     install_requires=[
         "policyengine_us==1.55.0",
         "tables",  # required by policyengine_us
-        "taxcalc>=4.3.4",
+        "taxcalc>=4.3.5",
         "scikit-learn",
         "torch",
         "tensorboard",
