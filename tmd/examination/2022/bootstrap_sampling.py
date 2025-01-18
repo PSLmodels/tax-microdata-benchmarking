e37e8996b15e3b8a7ca499bfb3ef795569eed4c2
martin.holmer@gmail.com <martin.holmer@gmail.com>
2025-01-18 09:41:31 -0500

    Add coefficient of variation to bootstrap_sampling.py

--- tmd/examination/2022/bootstrap_sampling.py
+++ tmd/examination/2022/bootstrap_sampling.py
@@ -6,7 +6,7 @@ import sys
 import numpy as np
 import pandas as pd
 
-USAGE = "USAGE: python bootstrap_sampling.py tc_dump_output_file_name\n"
+USAGE = "USAGE: python bootstrap_sampling.py tc_dump_output_csv_file_name\n"
 
 BS_SAMPLES = 1000
 BS_RNSEED = 192837465
@@ -41,20 +41,26 @@ def bootstrap_sampling(outfile):
         itax_list.append(itax_value)
     wght = np.sort(np.array(wght_list))
     itax = np.sort(np.array(itax_list))
+    wght_mean = wght.mean()
+    wght_stdv = wght.std()
+    wght_cv = wght_stdv / wght_mean
     print(
-        f"BS:wght num,mean,stdev = {BS_SAMPLES:4d}  "
-        f"{wght.mean():9.3f}  {wght.std():7.3f}"
+        f"BS:wght num,mean,stdev,cv(%) = {BS_SAMPLES:4d}  "
+        f"{wght_mean:9.3f}  {wght_stdv:7.3f}  {100 * wght_cv:6.2f}"
     )
     if BS_CI and BS_SAMPLES == 1000:
         print(f"BS:wght median = {wght[499]:9.3f}")
-        print(f"BS:wght 95%_conf_intv = {wght[24]:9.3f} , {wght[974]:9.3f}")
+        print(f"BS:wght 95%_conf_intv = {wght[24]:9.3f}  , {wght[974]:9.3f}")
+    itax_mean = itax.mean()
+    itax_stdv = itax.std()
+    itax_cv = itax_stdv / itax_mean        
     print(
-        f"BS:itax num,mean,stdev = {BS_SAMPLES:4d}  "
-        f"{itax.mean():9.3f}  {itax.std():7.3f}"
+        f"BS:itax num,mean,stdev,cv(%) = {BS_SAMPLES:4d}  "
+        f"{itax_mean:9.3f}  {itax_stdv:7.3f}  {100 * itax_cv:6.2f}"
     )
     if BS_CI and BS_SAMPLES == 1000:
         print(f"BS:itax median = {itax[499]:9.3f}")
-        print(f"BS:itax 95%_conf_intv = {itax[24]:9.3f} , {itax[974]:9.3f}")
+        print(f"BS:itax 95%_conf_intv = {itax[24]:9.3f}  , {itax[974]:9.3f}")
 
     return 0
 
