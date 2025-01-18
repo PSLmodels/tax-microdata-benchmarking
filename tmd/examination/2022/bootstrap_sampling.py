290c82607202e27c1a7ca3941d8cc21d7bf73aef
martin.holmer@gmail.com <martin.holmer@gmail.com>
2025-01-18 11:28:24 -0500

    Add flexibility to bootstrap_sampling.py script

--- tmd/examination/2022/bootstrap_sampling.py
+++ tmd/examination/2022/bootstrap_sampling.py
@@ -8,6 +8,9 @@ import pandas as pd
 
 USAGE = "USAGE: python bootstrap_sampling.py tc_dump_output_csv_file_name\n"
 
+SS_FRAC = 1.0
+SS_RNSEED = 902345678
+
 BS_SAMPLES = 1000
 BS_RNSEED = 192837465
 BS_DUMP = False
@@ -18,21 +21,26 @@ def bootstrap_sampling(outfile):
     """
     High-level logic of the script.
     """
-    # read odf from outfile
+    # read odf from outfile and possibly sample it
     odf = pd.read_csv(outfile)
-    print("len(odf) =", len(odf))
-    fdf = odf[(odf["data_source"] == 1) & (odf["iitax"] > 0)]
-    print("len(fdf) =", len(fdf))
-    print(f"FILE wght (#M) = {fdf['s006'].sum() * 1e-6:.3f}")
-    print(f"FILE itax ($B) = {(fdf['s006'] * fdf['iitax']).sum() * 1e-9:.3f}")
+    gdf = odf[(odf["data_source"] == 1) & (odf["iitax"] > 0)]
+    print("len(odf),len(gdf) =", len(odf), len(gdf))
+    if SS_FRAC < 1.0:
+        ss_rng = np.random.default_rng(seed=SS_RNSEED)
+        fdf = gdf.sample(frac=SS_FRAC, replace=True, random_state=ss_rng)
+    else:
+        fdf = gdf
+    print(f"SS_FRAC = {SS_FRAC:.2f}")
+    print(f"SS:wght(#M) = {fdf['s006'].sum() * 1e-6:.3f}")
+    print(f"SS:itax($B) = {(fdf['s006'] * fdf['iitax']).sum() * 1e-9:.3f}")
 
     # compute sum of wght and wght*itax for each bootstrap sample
     xdf = pd.DataFrame({"wght": fdf["s006"], "itax": fdf["iitax"]})
-    rng = np.random.default_rng(seed=BS_RNSEED)
+    bs_rng = np.random.default_rng(seed=BS_RNSEED)
     wght_list = []
     itax_list = []
     for bss in range(1, BS_SAMPLES + 1):
-        bsdf = xdf.sample(n=len(xdf), replace=True, random_state=rng)
+        bsdf = xdf.sample(n=len(xdf), replace=True, random_state=bs_rng)
         wght_value = bsdf["wght"].sum() * 1e-6
         itax_value = (bsdf["wght"] * bsdf["itax"]).sum() * 1e-9
         if BS_DUMP:
@@ -53,7 +61,7 @@ def bootstrap_sampling(outfile):
         print(f"BS:wght 95%_conf_intv = {wght[24]:9.3f}  , {wght[974]:9.3f}")
     itax_mean = itax.mean()
     itax_stdv = itax.std()
-    itax_cv = itax_stdv / itax_mean        
+    itax_cv = itax_stdv / itax_mean
     print(
         f"BS:itax num,mean,stdev,cv(%) = {BS_SAMPLES:4d}  "
         f"{itax_mean:9.3f}  {itax_stdv:7.3f}  {100 * itax_cv:6.2f}"
