5322735125e26412dcb77b1e6d67c49efeba9226
martinholmer <martin.holmer@gmail.com>
2026-03-09 17:18:08 -0400

    Eliminate double assignment in soi_replications.py code

--- tmd/utils/soi_replication.py
+++ tmd/utils/soi_replication.py
@@ -2,9 +2,9 @@ import pandas as pd
 import taxcalc as tc
 
 
-def tc_to_soi(puf, year):
-    policy = tc.Policy()
-    data = tc.Records(
+def tc_to_soi(puf: pd.DataFrame, year: int) -> pd.DataFrame:
+    pol = tc.Policy()
+    rec = tc.Records(
         data=puf,
         start_year=year,
         gfactors=None,
@@ -12,53 +12,52 @@ def tc_to_soi(puf, year):
         adjust_ratios=None,
         exact_calculations=True,
     )
-    calculator = tc.Calculator(policy=policy, records=data)
+    calculator = tc.Calculator(policy=pol, records=rec)
     calculator.calc_all()
-    puf = calculator.dataframe(None, all_vars=True)
+    pdf = calculator.dataframe(None, all_vars=True)
 
-    puf.columns = puf.columns.str.upper()
+    pdf.columns = pdf.columns.str.upper()
 
     df = pd.DataFrame()
-    df["adjusted_gross_income"] = puf.C00100
-    df["total_income_tax"] = puf.C09200 - puf.REFUND
-    df["employment_income"] = puf.E00200
-    df["capital_gains_distributions"] = puf.E01100
-    df["capital_gains_gross"] = puf["C01000"] * (puf["C01000"] > 0)
-    df["capital_gains_losses"] = -puf["C01000"] * (puf["C01000"] < 0)
-    df["estate_income"] = 0  # puf.E26390 Not in TC
-    df["estate_losses"] = 0  # puf.E26400
-    df["exempt_interest"] = puf.E00400
-    df["ira_distributions"] = puf.E01400
-    df["count_of_exemptions"] = puf.XTOT
-    df["ordinary_dividends"] = puf.E00600
-    df["partnership_and_s_corp_income"] = puf.E26270 * (puf.E26270 > 0)
-    df["partnership_and_s_corp_losses"] = -puf.E26270 * (puf.E26270 < 0)
-    df["total_pension_income"] = puf.E01500
-    df["taxable_pension_income"] = puf.E01700
-    df["qualified_dividends"] = puf.E00650
-    df["rent_and_royalty_net_income"] = 0  # puf.E25850 Not in TC
-    df["rent_and_royalty_net_losses"] = 0  # puf.E25860
-    df["total_social_security"] = puf.E02400
-    df["taxable_social_security"] = puf.C02500
-    df["income_tax_before_credits"] = puf.C05800
-    df["taxable_interest_income"] = puf.E00300
-    df["unemployment_compensation"] = puf.E02300
-    df["employment_income"] = puf.E00200
-    df["charitable_contributions_deduction"] = puf.C19700
-    df["interest_paid_deductions"] = puf.E19200
-    df["medical_expense_deductions_uncapped"] = puf.E17500
-    df["itemized_state_income_and_sales_tax_deductions"] = puf.E18400
-    df["itemized_real_estate_tax_deductions"] = puf.E18500
-    df["state_and_local_tax_deductions"] = puf.E18400 + puf.E18500
-    df["income_tax_after_credits"] = puf.IITAX
-    df["business_net_profits"] = puf.E00900 * (puf.E00900 > 0)
-    df["business_net_losses"] = -puf.E00900 * (puf.E00900 < 0)
-    df["qualified_business_income_deduction"] = puf.QBIDED
-    df["taxable_income"] = puf.C04800
-    df["is_tax_filer"] = puf.DATA_SOURCE == 1
-    df["is_taxable"] = puf.C09200 - puf.REFUND > 0
+    df["adjusted_gross_income"] = pdf.C00100
+    df["total_income_tax"] = pdf.C09200 - pdf.REFUND
+    df["employment_income"] = pdf.E00200
+    df["capital_gains_distributions"] = pdf.E01100
+    df["capital_gains_gross"] = pdf["C01000"] * (pdf["C01000"] > 0)
+    df["capital_gains_losses"] = -pdf["C01000"] * (pdf["C01000"] < 0)
+    df["estate_income"] = 0  # pdf.E26390 (not in T-C)
+    df["estate_losses"] = 0  # pdf.E26400 (not in T-C)
+    df["exempt_interest"] = pdf.E00400
+    df["ira_distributions"] = pdf.E01400
+    df["count_of_exemptions"] = pdf.XTOT
+    df["ordinary_dividends"] = pdf.E00600
+    df["partnership_and_s_corp_income"] = pdf.E26270 * (pdf.E26270 > 0)
+    df["partnership_and_s_corp_losses"] = -pdf.E26270 * (pdf.E26270 < 0)
+    df["total_pension_income"] = pdf.E01500
+    df["taxable_pension_income"] = pdf.E01700
+    df["qualified_dividends"] = pdf.E00650
+    df["rent_and_royalty_net_income"] = 0  # pdf.E25850 (not in T-C)
+    df["rent_and_royalty_net_losses"] = 0  # pdf.E25860 (not in T-C)
+    df["total_social_security"] = pdf.E02400
+    df["taxable_social_security"] = pdf.C02500
+    df["income_tax_before_credits"] = pdf.C05800
+    df["taxable_interest_income"] = pdf.E00300
+    df["unemployment_compensation"] = pdf.E02300
+    df["charitable_contributions_deduction"] = pdf.C19700
+    df["interest_paid_deductions"] = pdf.E19200
+    df["medical_expense_deductions_uncapped"] = pdf.E17500
+    df["itemized_state_income_and_sales_tax_deductions"] = pdf.E18400
+    df["itemized_real_estate_tax_deductions"] = pdf.E18500
+    df["state_and_local_tax_deductions"] = pdf.E18400 + pdf.E18500
+    df["income_tax_after_credits"] = pdf.IITAX
+    df["business_net_profits"] = pdf.E00900 * (pdf.E00900 > 0)
+    df["business_net_losses"] = -pdf.E00900 * (pdf.E00900 < 0)
+    df["qualified_business_income_deduction"] = pdf.QBIDED
+    df["taxable_income"] = pdf.C04800
+    df["is_tax_filer"] = pdf.DATA_SOURCE == 1
+    df["is_taxable"] = pdf.C09200 - pdf.REFUND > 0
     df["count"] = 1
-    df["filing_status"] = puf.MARS.map(
+    df["filing_status"] = pdf.MARS.map(
         {
             0: "SINGLE",  # Assume the aggregate record is single
             1: "SINGLE",
@@ -67,5 +66,5 @@ def tc_to_soi(puf, year):
             4: "HEAD_OF_HOUSEHOLD",
         }
     )
-    df["weight"] = puf["S006"]
+    df["weight"] = pdf["S006"]
     return df
